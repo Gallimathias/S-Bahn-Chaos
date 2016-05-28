b51e3bc5753c1f12b820f152ec645bd619c88d26
MarcusAurelius <Marcus.Aurelius@gallimathias.de>
2016-05-28 21:59:11 +0200

    Add Winform for Controling

--- SBahn-Chaos-WorkplaceTool/SBahn-Chaos-WorkplaceTool.Core/Properties/AssemblyInfo.cs
+++ SBahn-Chaos-WorkplaceTool/SBahn-Chaos-WorkplaceTool.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("cdf52c31-383f-4bdb-87f2-ff4641cefbbe")]
+[assembly: Guid("6e6fbe37-b745-430e-b9f4-fe05588a1a9d")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
